da534d41f26b45e8b8202a0411e42307f562519b
loregorza <lorenzogorza@yahoo.it>
2024-10-28 10:22:09 +0000

    updated transit.py code

---
+++ src/daneel/detection/transit.py
@@ -0,0 +1,29 @@
+import batman
+import numpy as np
+import matplotlib.pyplot as plt
+
+time = np.linspace(-0.05, 0.05, 1000)  # time array in days (centered on transit)
+
+params = batman.TransitParams()       # object to hold parameters
+params.t0 = 0                         # time of inferior conjunction
+params.per = 1.0                      # orbital period in days
+params.rp = 0.1                       # planet radius (in units of stellar radii)
+params.a = 15                         # semi-major axis (in units of stellar radii)
+params.inc = 87.                       # orbital inclination (in degrees)
+params.ecc = 0                        # eccentricity
+params.w = 90                         # longitude of periastron (in degrees)
+params.limb_dark = "quadratic"        # limb darkening model
+params.u = [0.1, 0.3]                 # limb darkening coefficients
+
+m = batman.TransitModel(params, time)  # initializes model
+flux = m.light_curve(params)           # calculates light curve
+
+fig=plt.figure(figsize=(10, 6))
+plt.plot(time, flux, color="blue", label="Model Light Curve")
+plt.xlabel("Time from transit center (days)")
+plt.ylabel("Relative flux")
+plt.title("Exoplanet Transit Light Curve")
+plt.legend()
+plt.grid(True)
+plt.show()
+fig.savefig('/root/2_veneti_e_mezzo/src/daneel/detection/Kepler-22_b_assignment1_taskF.png', dpi=fig.dpi)
