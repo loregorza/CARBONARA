b7d510ab53b42dacb51c62ca7dd309e96f386308
loregorza <lorenzogorza@yahoo.it>
2024-11-11 09:24:02 +0000

    updating other stuff

--- src/daneel/detection/transit.py
+++ src/daneel/detection/transit.py
@@ -19,10 +19,11 @@ m = batman.TransitModel(params, time)    #initializes model
 flux = m.light_curve(params)           # calculates light curve
 
 fig=plt.figure(figsize=(10, 6))
-plt.plot(time, flux, color="teal", label="Model Light Curve")
-plt.xlabel("Time from transit center (days)")
+plt.plot(time, flux, color="salmon", label="Model Light Curve")
+plt.xlabel("Time from transit center (Julian days)")
 plt.ylabel("Relative flux")
 plt.title("Kepler-22 b Transit Light Curve")
 plt.legend()
 plt.show()
-fig.savefig('/root/2_veneti_e_mezzo/src/daneel/detection/Kepler-22_b_assignment1_taskF.png', dpi=fig.dpi)
+#fig.savefig('/root/2_veneti_e_mezzo/src/daneel/detection/Kepler-22_b_assignment1_taskF.png', dpi=fig.dpi) #task1F
+fig.savefig('/root/2_veneti_e_mezzo/src/daneel/detection/assignment2_taskA.png', dpi=fig.dpi)  #task 2A
